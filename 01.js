06a5d54c3ddacbbe093da6c69509861e7bf77ea9
Tyoma0 <artyom0070071@mail.ru>
2024-08-19 10:39:38 +0300

    dsd

--- 01.js
+++ 01.js
@@ -3,21 +3,11 @@
 // your code = 0
 
 
-let number = 0;
-
-
-let found = false;
-
-
-while (!found) {
-   
-    number = prompt("Введите число от 1 до 20: ");
-
-    
-    if (parseInt(number) >= 1 && parseInt(number) <= 20) {
-        console.log(`Вы ввели число ${number}, оно находится в требуемом диапазоне.`);
-        found = true;
-    } else {
-        console.log(`Вы ввели число ${number}, которое не находится в требуемом диапазоне (от 1 до 20).`);
-    }
+let sum = 0; 
+let i = 1;
+while (i <= 20) { 
+    sum += i; 
+    i++; 
 }
+
+console.log(sum); 
